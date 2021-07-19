8f031242d75b5c70ac08610670eb4b0ec9a72cd6
Ri Le <rl2999@columbia.edu>
2021-07-19 11:34:20 -0400

    added in register view

---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Search for a work.
+
+    <Search/>
+  </div>
+</template>
+
+<script>
+
+/*
+ * @ is an alias to /src
+ * import HelloWorld from '@/components/HelloWorld.vue'
+ */
+import Search from './Search.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Search
+  }
+};
+</script>
