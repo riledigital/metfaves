da729ff95d9d29d58361acd5bbf2529a108e7727
Ri Le <rl2999@columbia.edu>
2021-07-19 11:34:27 -0400

    add registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,23 +1,63 @@
 <template>
   <div>
-    Search for a work.
+    Register for an account!
 
-    <Search/>
+    <form @submit.prevent @submit="registerApi" class="form">
+      <input v-model="name" placeholder="Name" />
+      <input v-model="email" placeholder="Email" />
+      <input v-model="bio" placeholder="Bio" />
+      {{ response.status }} {{ response }}
+      <button>Register</button>
+    </form>
   </div>
 </template>
 
 <script>
-
 /*
  * @ is an alias to /src
  * import HelloWorld from '@/components/HelloWorld.vue'
  */
-import Search from './Search.vue';
 
 export default {
   name: 'Home',
-  components: {
-    Search
+  data: function() {
+    return {
+      name: '',
+      email: '',
+      bio: '',
+      response: ''
+    };
+  },
+  components: {},
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.registerApi();
+    },
+    async registerApi() {
+      const postBody = { name: this.name, email: this.email, bio: this.bio };
+      const resp = await fetch(process.env.VUE_APP_API_BASE + '/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        // mode: 'no-cors',
+        body: JSON.stringify(postBody)
+      });
+      const data = resp;
+      console.log(data);
+      this.response = await data.text();
+    }
   }
 };
 </script>
+
+<style scoped>
+.form {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  max-width: 50vw;
+  gap: 1rem;
+}
+</style>
